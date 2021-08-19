b172b10943df43ea1c626cba375c5afa5721e3eb
Clancinio <dean.clancy91@gmail.com>
2021-08-19 21:15:30 +0100

    Init commit

---
+++ src/main/java/com/deanclancydev/backendproductmanagement/BackendProductManagementApplication.java
@@ -0,0 +1,13 @@
+package com.deanclancydev.backendproductmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProductManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deanclancydev/backendproductmanagement/BackendProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deanclancydev.backendproductmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
