d2f2e6242b5c327b49b9a6e38dfb48e8db852779
Clancinio <dean.clancy91@gmail.com>
2021-08-20 18:54:11 +0100

    Add Product Entity

---
+++ src/main/java/com/deanclancydev/backendproductmanagement/entity/Product.java
@@ -0,0 +1,26 @@
+package com.deanclancydev.backendproductmanagement.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private String price;
+
+
+}
