e2cd126fe019424f850c4fb49d261b5274b6cf39
Clancinio <dean.clancy91@gmail.com>
2021-08-20 18:46:30 +0100

    Add user entity

---
+++ src/main/java/com/deanclancydev/backendproductmanagement/entity/User.java
@@ -0,0 +1,22 @@
+package com.deanclancydev.backendproductmanagement.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
+
