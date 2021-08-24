271f76ece304b7bc60728c142e18ab88c5edc9e7
Clancinio <dean.clancy91@gmail.com>
2021-08-24 18:17:31 +0100

    Add Product and Repository repos

---
+++ src/main/java/com/deanclancydev/backendproductmanagement/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.deanclancydev.backendproductmanagement.repository;
+
+import com.deanclancydev.backendproductmanagement.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
