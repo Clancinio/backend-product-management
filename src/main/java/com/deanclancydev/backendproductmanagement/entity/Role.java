3067d66761af88b3fa9c9127e9bf3eb4b10cb5b1
Clancinio <dean.clancy91@gmail.com>
2021-09-03 12:22:53 +0100

    Clean project

--- src/main/java/com/deanclancydev/backendproductmanagement/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.deanclancydev.backendproductmanagement.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- src/main/java/com/deanclancydev/backendproductmanagement/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deanclancydev.backendproductmanagement.repository;
-
-import com.deanclancydev.backendproductmanagement.entity.TransactionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
-}
