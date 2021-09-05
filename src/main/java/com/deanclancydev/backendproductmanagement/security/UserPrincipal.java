262960f983cea12c2963625e6d9f818c6c58251b
Clancinio <dean.clancy91@gmail.com>
2021-09-05 18:50:41 +0100

    Create user principal

---
+++ src/main/java/com/deanclancydev/backendproductmanagement/security/UserPrincipal.java
@@ -0,0 +1,55 @@
+package com.deanclancydev.backendproductmanagement.security;
+
+import com.deanclancydev.backendproductmanagement.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private final UserEntity user;
+
+    @Override
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.user.getRole()
+                .getPermissions()
+                .stream().
+                map(auth -> new SimpleGrantedAuthority(auth.getPermission())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.user.getIsNotLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getIsActive();
+    }
+}
